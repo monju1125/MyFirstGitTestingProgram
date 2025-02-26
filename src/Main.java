24f52508c9bcad2c9efda18c641d0e2835df533b
monju1125 <csekarim86@gmail.com>
2025-02-25 19:53:22 -0500

    added new display..

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome. This Program is a Testing for Git Connection!");
+        addNewContent();
+        }
+
+        public static void addNewContent(){
+            System.out.println("This Method add new Display and Showing Changes to Git Repositor");
         }
     }
