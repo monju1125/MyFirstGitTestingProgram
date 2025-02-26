2acc60c1186263a8e3dc16ad20bdced43ce79d1b
monju1125 <csekarim86@gmail.com>
2025-02-25 21:32:20 -0500

    Assignment-6 Done and Submitted Through GitHub

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome. This Program is a Testing for Git Connection!");
-        addNewContent();
-        }
-
-        public static void addNewContent(){
-            System.out.println("This Method add new Display and Showing Changes to Git Repositor");
+        System.out.println("The Purpose of this Project is Test for Git Connection and Submission of Assignment# 6 using GitHub");
+        double[ ] JavaGPAofStudents= {2.4,3.4,2.3,3.8,4.0,3.5};
+        double[ ] PythonGPAofStudents = {3.4,2.4,2.1,3.7,4.0,2.5};
+        System.out.println("\nTotal Students who got GPA 3.5 or above in Java: " + countStudents(JavaGPAofStudents) );
+        System.out.println("\nTotal Students who got GPA 3.5 or above in Python: " + countStudents(PythonGPAofStudents) );
+        System.out.println("\nGenerating Random Number...");
+        int num = 0;
+        while( num < 7){
+             int Num = (int) Math.floor(Math.random()*11);
+            System.out.println( "Random Number is: "  + Num);
+            num++;
         }
     }
+    public static int  countStudents(double[ ] JavaGPAofStudents) {
+        int count = 0;
+        for (int i = 0; i < JavaGPAofStudents.length; i++) {
+            if (JavaGPAofStudents[i] >= 3.5 ) {
+                count+=1;
+            }
+        }
+        return count;
+    }
+}
